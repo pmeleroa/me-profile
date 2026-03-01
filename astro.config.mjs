db5408793ce2fb3a4b49213a915ae4e4356cc8c9
Pablo Melero Alonso <p.melero.alonso@gmail.com>
2026-03-01 08:41:47 +0100

    feat: integrate Google Analytics with Partytown for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://pmeleroa.github.io',
   base: '/me-profile/',
   compressHTML: true,
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
