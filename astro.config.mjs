9bb8225280c5b899374d2f28b227d3fb32c694ad
Pablo Melero Alonso <p.melero.alonso@gmail.com>
2026-03-01 21:02:20 +0100

    feat: update site URL in configuration for correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  site: 'https://pmeleroa.github.io',
+  site: 'https://pablomeleroalonso.me',
   base: '/me-profile/',
   compressHTML: true,
   integrations: [
